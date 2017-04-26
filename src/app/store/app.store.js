02827c3401bb2b6d5773bcb00e6564b59fadeb0f
nicogarcia <nicoalbo90@gmail.com>
2017-04-26 17:10:24 -0300

    Added persisted and rehydrated store

--- src/app/store/app.store.js
+++ src/app/store/app.store.js
@@ -1,13 +1,19 @@
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import thunkMiddleware from "redux-thunk";
 import appReducer from "../reducer/app.reducer";
+import {autoRehydrate, persistStore} from "redux-persist";
 
 let store = createStore(
     appReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(
-        thunkMiddleware
+    compose(
+        applyMiddleware(
+            thunkMiddleware
+        ),
+        autoRehydrate()
     )
 );
+
+persistStore(store);
 
 export default store;
