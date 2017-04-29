1afd7563499a962c11b3d2c4e22a2727b746d984
nicogarcia <nicoalbo90@gmail.com>
2017-04-29 19:50:27 -0300

    Refactored out CreateTripModal component

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Redirect, Route, Switch, withRouter} from "react-router";
+import {Redirect, Route, Switch} from "react-router";
 
 import Header from "../header/Header";
 import Login from "../login/Login";
@@ -8,15 +8,9 @@ import Trips from "../trip/TripList";
 
 import "./App.css";
 import PrivateRoute from "./router/PrivateRoute";
-import {Button, Col, Form, FormGroup, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
-import PlaceAutocomplete from "../places/PlaceAutocomplete";
-import {closeCreateModalAction} from "../trip/create-modal/action/action.types";
-import {connect} from "react-redux";
+import CreateTripModal from "../trip/create-modal/CreateTripModal";
 
 class App extends Component {
-    closeCreateTripModal = () => {
-        this.props.dispatch(closeCreateModalAction());
-    };
 
     render() {
         return (
@@ -30,41 +24,10 @@ class App extends Component {
                     <PrivateRoute path="/trips" component={Trips}/>
                 </Switch>
 
-                <Modal isOpen={this.props.createTripModal.opened}>
-                    <ModalHeader>Find your trip</ModalHeader>
-                    <ModalBody>
-
-                        <Form>
-
-                            <FormGroup row>
-                                <Label sm={2}>From</Label>
-                                <Col sm={10}>
-                                    <PlaceAutocomplete host="create.from"/>
-                                </Col>
-                            </FormGroup>
-
-                            <FormGroup row>
-                                <Label sm={2}>To</Label>
-                                <Col sm={10}>
-                                    <PlaceAutocomplete host="create.to"/>
-                                </Col>
-                            </FormGroup>
-
-                        </Form>
-
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="secondary" onClick={this.closeCreateTripModal}>Cancel</Button>
-                        <Button color="primary">Book Trip</Button>
-                    </ModalFooter>
-                </Modal>
+                <CreateTripModal />
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => ({
-    createTripModal: state.createTripModal
-});
-
-export default withRouter(connect(mapStateToProps)(App));
+export default App;
---
+++ src/trip/create-modal/CreateTripModal.js
@@ -0,0 +1,64 @@
+import React, {Component} from "react";
+import {Button, Col, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import {closeCreateModalAction} from "./action/action.types";
+import {createTrip} from "../action/action.types";
+import {connect} from "react-redux";
+
+class CreateTripModal extends Component {
+
+    input;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    closeCreateTripModal = () => {
+        this.props.dispatch(closeCreateModalAction());
+    };
+
+    createTripHandler = (e) => {
+        e.preventDefault();
+
+        if (!this.state.value.trim()) {
+            return
+        }
+
+        this.props.dispatch(createTrip({name: this.state.value}));
+
+        this.setState({value: ''});
+    };
+
+    render() {
+        return (
+            <Modal isOpen={this.props.createTripModal.opened}>
+                <ModalHeader>Create a new trip</ModalHeader>
+
+                <Form onSubmit={this.createTripHandler}>
+                    <ModalBody>
+                        <FormGroup row>
+                            <Label sm={2}>Name</Label>
+                            <Col sm={10}>
+                                <Input value={this.state.value} onChange={e => this.setState({value: e.target.value})}/>
+                            </Col>
+                        </FormGroup>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.closeCreateTripModal}>Cancel</Button>
+                        <Button color="primary" type="submit">Book Trip</Button>
+                    </ModalFooter>
+                </Form>
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    createTripModal: state.createTripModal
+});
+
+export default connect(mapStateToProps)(CreateTripModal);
