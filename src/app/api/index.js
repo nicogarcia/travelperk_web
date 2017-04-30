4ff6da3c4f0af47846dfaf8c4e350747079268bb
nicogarcia <nicoalbo90@gmail.com>
2017-04-30 00:09:57 -0300

    Added Authorization header to Api

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,6 +1,21 @@
 import axios from "axios";
+import store from "../store/app.store";
 
-export const Api = axios.create({
+const instance = axios.create({
     baseURL: 'http://localhost:8000/v1',
-    timeout: 2000
+    timeout: 4000
 });
+
+store.subscribe(() => {
+    const token = store.getState().login.token;
+    console.log('token = ' + token);
+
+    if (token) {
+        instance.defaults.headers.common['Authorization'] = 'JWT ' + token;
+    } else {
+        delete instance.defaults.headers.common['Authorization'];
+    }
+
+});
+
+export const Api = instance;
